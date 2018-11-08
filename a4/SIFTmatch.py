e10a841ddd0032826e05503a7691858ed33cec27
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2018-11-08 11:11:10 -0800

    fix for python 3

--- a4/SIFTmatch.py
+++ a4/SIFTmatch.py
@@ -3,7 +3,8 @@ import numpy as np
 import csv
 import math
 
-def ReadKeys(image):
+
+def ReadKeys(image: Image) -> list:
     """Input an image and its associated SIFT keypoints.
 
     The argument image is the image file name (without an extension).
@@ -22,12 +23,12 @@ def ReadKeys(image):
     for one of the K keypoints.  The descriptor is a 1D array of 128
     values with unit length.
     """
-    im = Image.open(image+'.pgm').convert('RGB')
+    im = Image.open(image + '.pgm').convert('RGB')
     keypoints = []
     descriptors = []
     first = True
-    with open(image+'.key','rb') as f:
-        reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONNUMERIC,skipinitialspace = True)
+    with open(image + '.key', 'r') as f:
+        reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONNUMERIC, skipinitialspace=True)
         descriptor = []
         for row in reader:
             if len(row) == 2:
@@ -42,14 +43,15 @@ def ReadKeys(image):
             if len(row) == 8:
                 descriptor += row
                 assert len(descriptor) == 128, "Keypoint descriptor length invalid (should be 128)."
-                #normalize the key to unit length
+                # normalize the key to unit length
                 descriptor = np.array(descriptor)
-                descriptor = descriptor / math.sqrt(np.sum(np.power(descriptor,2)))
+                descriptor = descriptor / math.sqrt(np.sum(np.power(descriptor, 2)))
                 descriptors.append(descriptor)
                 descriptor = []
     assert len(keypoints) == count, "Incorrect total number of keypoints read."
-    print "Number of keypoints read:", int(count)
-    return [im,keypoints,descriptors]
+    print("Number of keypoints read:", int(count))
+    return [im, keypoints, descriptors]
+
 
 def AppendImages(im1, im2):
     """Create a new image that appends two images side-by-side.
@@ -58,10 +60,11 @@ def AppendImages(im1, im2):
     """
     im1cols, im1rows = im1.size
     im2cols, im2rows = im2.size
-    im3 = Image.new('RGB', (im1cols+im2cols, max(im1rows,im2rows)))
-    im3.paste(im1,(0,0))
-    im3.paste(im2,(im1cols,0))
+    im3 = Image.new('RGB', (im1cols + im2cols, max(im1rows, im2rows)))
+    im3.paste(im1, (0, 0))
+    im3.paste(im2, (im1cols, 0))
     return im3
+
 
 def DisplayMatches(im1, im2, matched_pairs):
     """Display matches on a new image with the two input images placed side by side.
@@ -73,15 +76,16 @@ def DisplayMatches(im1, im2, matched_pairs):
 
     Displays and returns a newly created image (in PIL 'RGB' format)
     """
-    im3 = AppendImages(im1,im2)
+    im3 = AppendImages(im1, im2)
     offset = im1.size[0]
     draw = ImageDraw.Draw(im3)
     for match in matched_pairs:
-        draw.line((match[0][1], match[0][0], offset+match[1][1], match[1][0]),fill="red",width=2)
+        draw.line((match[0][1], match[0][0], offset + match[1][1], match[1][0]), fill="red", width=2)
     im3.show()
     return im3
 
-def match(image1,image2):
+
+def match(image1: str, image2: str) -> Image:
     """Input two images and their associated SIFT keypoints.
     Display lines connecting the first 5 keypoints from each image.
     Note: These 5 are not correct matches, just randomly chosen points.
@@ -97,17 +101,17 @@ def match(image1,image2):
     #
     # REPLACE THIS CODE WITH YOUR SOLUTION (ASSIGNMENT 5, QUESTION 3)
     #
-    #Generate five random matches (for testing purposes)
+    # Generate five random matches (for testing purposes)
     matched_pairs = []
     num = 5
     for i in range(num):
-        matched_pairs.append([keypoints1[i],keypoints2[i]])
+        matched_pairs.append([keypoints1[i], keypoints2[i]])
     #
     # END OF SECTION OF CODE TO REPLACE
     #
     im3 = DisplayMatches(im1, im2, matched_pairs)
     return im3
 
-#Test run...
-match('scene','basmati')
 
+# Test run...
+match('scene', 'basmati')
